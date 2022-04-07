c10254738486981b803edfa0159fc4ba644d84a6
Harrison Kleiman <harrikleiman@gmail.com>
2022-04-07 16:20:45 -0400

    rollbar rocking

--- server/index.js
+++ server/index.js
@@ -5,15 +5,34 @@ const path = require("path")
 
 //rollbar
 
+
+
+
+
+
 app.use(express.json())
 
 // Set server port
 const port = process.env.port || 4005
 
-
 // Rollbar installation
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar")
+var rollbar = new Rollbar({
+  accessToken: "374377d6c65d4e6180ee935a71221deb",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!")
 
+// create a new instance of it using your post_server_item access token
+const rollbar2 = new Rollbar({
+  accessToken: "374377d6c65d4e6180ee935a71221deb",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 // endpoints
 app.get("/", (req, res) => {
@@ -22,4 +41,14 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is rocking on port ${port}`)
+})
+
+// Add an endpoint and in the callback function, call a function that does not exist so you can test out Rollbar (hint: try using a try…catch block
+app.get("/error", (req, res) => {
+  try {
+    // throw new Error("This is a test error")
+    res.send("Hello World... again!")
+  } catch (err) {
+    rollbar2.log(err)
+  }
 })
